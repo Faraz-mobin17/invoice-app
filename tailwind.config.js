6fd4ef85d0942bfde07a5710258a264333e4a247
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-01-02 00:42:48 +0530

    updated indx and tailwind file with center prop and text colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      black: "#0C0E16",
+      gray: "#7E88C3",
+      lightGray: "#858BB2",
+      green: "#33D69F",
+      orange: "#FF8F00",
+      purple: "#7C5DFA",
+    },
     extend: {},
   },
   plugins: [],
