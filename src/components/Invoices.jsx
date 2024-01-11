09634946609514d1d1f709442a33a70dc1c49e07
Faraz-mobin17 <faraz.mobin17@gmail.com>
2024-01-11 22:36:40 +0530

    ✅ Invoice component

--- src/components/Invoices.jsx
+++ src/components/Invoices.jsx
@@ -1,17 +1,45 @@
+// Invoices.jsx
 import React from "react";
+import { IconRight } from "./icons_components/index";
 
-function Invoices() {
+// import IconSun from "./IconSun";
+function Invoices({ id, paymentDue, clientName, price, status }) {
+  const getStatusColor = () => {
+    if (status === "paid") {
+      return "text-green";
+    } else if (status === "pending") {
+      return "text-orange";
+    } else {
+      return "text-black font-bold";
+    }
+  };
+
   return (
-    <div className="invoice-container flex justify-evenly">
-      <div>#RT3080</div>
-      <div>Due 19 Aug</div>
-      <div>John Doe</div>
-      <div>$1000</div>
-      <div>Paid</div>
-      <div>
-        <span>&gt;</span>
+    <>
+      <div className="invoice-container flex justify-evenly shadow-customShadow mt-4 p-6 rounded-[8px] items-center align-middle w-[548px] ">
+        <div className="font-bold text-black text-[15px] ">
+          <span className="text-gray">#</span>
+          {id}
+        </div>
+        <div className="text-gray font-light text-[13px]">
+          Due on {paymentDue}
+        </div>
+        <div className="text-lightGray font-light text-[13px]">
+          {clientName}
+        </div>
+        <div className="font-bold text-black">${price}</div>
+        <div
+          className={`font-bold capitalize rounded bg-[#F3FDFA] p-2 w-[104px] h-[40px] text-center ${getStatusColor()}`}
+        >
+          {status === "draft" ? "Draft" : <span>{status}</span>}
+        </div>
+        <div>
+          <span>
+            <IconRight />
+          </span>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
 
